1a98f6cf93913eaa22734df12eb88c2a2b025a9c
Jasper van der Linden <jappe@pop-os.localdomain>
2019-09-27 13:27:22 +0200

    feat: Keep track of game creators

--- server/src/cards/controller/cards.controller.ts
+++ server/src/cards/controller/cards.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { CardViewDto } from '../card.dto'
 import { CardsService } from '../service/cards.service'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('api/cards')
 export class CardsController {
   constructor(private cardsService: CardsService) {}
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   fetchAll(@Query() query: { [key: string]: any }): Promise<CardViewDto[]> {
     return this.cardsService.findAll(query)
   }
