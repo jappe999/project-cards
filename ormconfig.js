68461220fbf3606b109580cf9514e1426b14ef23
Panda <daniel.wezeman@vizzlo.com>
2019-10-03 14:31:32 +0200

    fix: Put credentials in .env file

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,9 @@
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+const name = process.env.DB_NAME
+
 module.exports = {
   type: 'postgres',
   keepConnectionAlive: true,
@@ -5,10 +11,7 @@ module.exports = {
     process.env.NODE_ENV === 'production'
       ? { rejectUnauthorized: false }
       : false,
-  url:
-    process.env.NODE_ENV === 'production'
-      ? 'postgres://zjevarcywtpjoi:6bbf1f577abb7ade0df29c71c4b2d57c7b207ffb695b205c949241540d79af76@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/ddnmseo8fmepc1'
-      : 'postgres://root:root@psql.localhost:5433/projectcards',
+  url: `postgres://${user}:${pass}@${host}:${port}/${name}`,
   synchronize: true,
   seeds: ['server/database/seeders/**/*.seeder.ts'],
   factories: ['server/database/factories/**/*.factory.ts'],
