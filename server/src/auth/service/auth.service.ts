b9ca8f9bcba0e135425725e597afe7650e3a7b45
Jasper van der Linden <jappe@pop-os.localdomain>
2019-10-08 17:21:50 +0200

    fix: Add bcrypt for password hashing and comparing

--- server/src/auth/service/auth.service.ts
+++ server/src/auth/service/auth.service.ts
@@ -4,9 +4,13 @@ import { verify, decode } from 'jsonwebtoken'
 import { UsersService } from '../../users/service/users.service'
 import { UserViewDto, UserCreateDto } from '../../users/user.dto'
 import { Socket } from 'socket.io'
+import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
+  private static readonly SALT_ROUNDS = 10
+
+  private static readonly ERROR_USER_REGISTRATION = 'Cannot register user.'
   private static readonly ERROR_USER_LOGGED_IN = 'User already logged in.'
   private static readonly ERROR_LOGIN_INCORRECT = 'User or password incorrect.'
 
@@ -47,7 +51,7 @@ export class AuthService {
         return AuthService.ERROR_USER_LOGGED_IN
       }
 
-      if (user.password === pass) {
+      if (await this.validatePassword(user, pass)) {
         const { password, ...result } = user
         return result
       }
@@ -62,9 +66,24 @@ export class AuthService {
     return this.usersService.findOne({ where: { id } })
   }
 
-  async register(user: UserCreateDto): Promise<UserViewDto> {
-    const { password, ...result } = await this.usersService.create(user)
-    return result
+  async register(user: UserCreateDto): Promise<UserViewDto | string> {
+    try {
+      user.password = await bcrypt.hash(user.password, AuthService.SALT_ROUNDS)
+      const { password, ...result } = await this.usersService.create(user)
+      return result
+    } catch (error) {
+      return AuthService.ERROR_USER_REGISTRATION
+    }
+  }
+
+  async validatePassword(
+    user: UserCreateDto,
+    password: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(
+      password, // Unhashed password
+      user.password, // Hashed password
+    )
   }
 
   async login({ id, username }: any) {
